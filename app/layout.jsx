690ebe5c4fa174bfba7a94bd8b368b6d145dbd01
luismdg <luism.delgadillo1234@gmail.com>
2026-02-09 19:04:36 -0600

    SEO very basic

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,14 @@ import Header from "./sections/header";
 import Footer from "./sections/footer";
 import { BackgroundElements } from "./ui/BackgroundElements";
 
+export const metadata = {
+  title: 'Cliro',
+  description: 'Cliro WebsiteCliro, an AI writing companion that boosts creativity when words don’t come easily.',
+  icons: {
+    icon: '/favicon.png',
+  },
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
